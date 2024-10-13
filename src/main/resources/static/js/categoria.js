9af632a27b06a9d82e8e3891d06e9bee6ee29628
tmacoppi <tmacoppi@gmail.com>
2024-10-13 15:05:35 -0300

    Inclusão Categoria

--- src/main/resources/static/js/categoria.js
+++ src/main/resources/static/js/categoria.js
@@ -1,81 +1,86 @@
 $(document).ready(
     function() {
+        ajaxListarCategoria();
+    });
 
-        // GET REQUEST
-        $("#listarCategoria").click(function(event) {
-            event.preventDefault();
+function ajaxListarCategoria() {
+    $('#getResultDiv').empty();
+    $.ajax({
+        type : "GET",
+        url : window.location + "categoria/",
+        success : function(result) {
+            montarTabelaResultado(result)
+            //$.toaster({ priority : 'success', title : 'Categoria', message : 'Categorias listadas com sucesso!'});
+        },
+        error : function(e) {
+            //$.toaster({ priority : 'danger', title : 'Categoria', message : e.responseText});
+            toast("Categoria", e.responseText, "error");
+            console.log("ERROR: ", e);
+        }
+    });
+}
+
+function montarTabelaResultado(result) {
+    const table = $('<table></table>').addClass('table'); // Create a table element
+    const thead = $('<thead></thead>');
+    const tbody = $('<tbody></tbody>');
+
+    // Create table header
+    thead.append('<tr><th>Ações</th><th>Nome</th></tr>');
+
+    // Create table rows from data
+    $.each(result, function(index, item) {
+        const row = $('<tr></tr>');
+
+        // Add actions column with icons and links
+        const actionsCell = $('<td></td>');
+        actionsCell.append(`<button type="button" class="btn btn-danger bi-trash" th:onclick="|location.href='/usuario/apagar/${item.id}'|" />`);
+        actionsCell.append(`<button type="button" class="btn btn-warning bi-pencil" th:onclick="|location.href='/usuario/editar/${item.id}'|" />`);
+        row.append(actionsCell);
+
+        row.append('<td>' + item.nome + '</td>');
+        tbody.append(row);
+    });
+
+    table.append(thead);
+    table.append(tbody);
+
+    $('#getResultDiv').html(table);
+
+}
+
+// SUBMIT FORM
+$("#categoriaForm").submit(function(event) {
+    // Prevent the form from submitting via the browser.
+    event.preventDefault();
+    ajaxPostCategoria();
+});
+
+function ajaxPostCategoria() {
+    console.log("ajaxPostCategoria");
+
+    // PREPARE FORM DATA
+    var formData = {
+        nome : $("#nome").val()
+    }
+
+    // DO POST
+    $.ajax({
+        type : "POST",
+        contentType : "application/json",
+        url : window.location + "categoria/gravar/",
+        data : JSON.stringify(formData),
+        dataType : 'json',
+        success : function(result) {
+            //$.toaster({ priority : 'success', title : 'Categoria', message : result.nome + ' gravada com sucesso!'});
+            toast("Categoria", result.nome + " gravada com sucesso!", "success");
             ajaxListarCategoria();
-        });
-
-        // DO GET
-        function ajaxListarCategoria() {
-            $('#getResultDiv').empty();
-            $.ajax({
-                type : "GET",
-                url : window.location + "categoria/",
-                success : function(result) {
-                    montarTabelaResultado(result)
-                    //$.toaster({ priority : 'success', title : 'Categoria', message : 'Categorias listadas com sucesso!'});
-                },
-                error : function(e) {
-                    $.toaster({ priority : 'danger', title : 'Categoria', message : e.responseText});
-                    console.log("ERROR: ", e);
-                }
-            });
+            console.log(result);
+        },
+        error : function(e) {
+            alert("Error!")
+            console.log("ERROR: ", e);
         }
+    });
 
-        function montarTabelaResultado(result) {
-            const table = $('<table></table>').addClass('table'); // Create a table element
-            const thead = $('<thead></thead>');
-            const tbody = $('<tbody></tbody>');
-
-            // Create table header
-            thead.append('<tr><th>ID</th><th>Nome</th></tr>');
-
-            // Create table rows from data
-            $.each(result, function(index, item) {
-                const row = $('<tr></tr>');
-                row.append('<td>' + item.id + '</td>');
-                row.append('<td>' + item.nome + '</td>');
-                tbody.append(row);
-            });
-
-            table.append(thead);
-            table.append(tbody);
-            $('#getResultDiv').html(table);
-        }
-
-        // SUBMIT FORM
-        $("#categoriaForm").submit(function(event) {
-            // Prevent the form from submitting via the browser.
-            event.preventDefault();
-            ajaxPostCategoria();
-        });
-
-        function ajaxPostCategoria() {
-
-            // PREPARE FORM DATA
-            var formData = {
-                nome : $("#nome").val()
-            }
-
-            // DO POST
-            $.ajax({
-                type : "POST",
-                contentType : "application/json",
-                url : window.location + "categoria/gravar/",
-                data : JSON.stringify(formData),
-                dataType : 'json',
-                success : function(result) {
-                    $.toaster({ priority : 'success', title : 'Categoria', message : result.nome + ' gravada com sucesso!'});
-                    ajaxListarCategoria();
-                    console.log(result);
-                },
-                error : function(e) {
-                    alert("Error!")
-                    console.log("ERROR: ", e);
-                }
-            });
-
-        }
-    })
+}
--- src/main/resources/static/js/estoque.js
+++ src/main/resources/static/js/estoque.js
@@ -1,3 +1,27 @@
 $.getScript("/js/categoria.js");
 $.getScript("/js/postrequest.js");
 $.getScript("/js/getrequest.js");
+
+
+function categoria(){
+    console.log("categoria");
+    $("#container").load("/templates/categoria.html");
+    return false;
+}
+
+function toast(titulo, mensagem, tipo) {
+    $.toast({
+        text: mensagem, // Text that is to be shown in the toast
+        heading: titulo, // Optional heading to be shown on the toast
+        icon: tipo, // Type of toast icon
+        showHideTransition: 'fade', // fade, slide or plain
+        allowToastClose: true, // Boolean value true or false
+        hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+        stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+        position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+
+        textAlign: 'left',  // Text alignment i.e. left, right or center
+        loader: true,  // Whether to show loader or not. True by default
+        loaderBg: '#9EC600'
+    });
+}
